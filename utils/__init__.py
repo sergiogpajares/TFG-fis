f24c1cb7da3f252a1a2e0bf2dda26418d4a82f78
Sergio <sergiogarciapajares@gmail.com>
2024-04-04 17:09:05 +0200

    ADDED DeepLabV3+

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import tensorflow as tf
 
 import os
 from typing import List, Tuple, Union
@@ -13,7 +14,8 @@ __name__ = "Utils"
 __all__ = [
     'display',
     'draw_grid',
-    'augmented_display'
+    'augmented_display',
+    'multipredict'
 ]
 
 def display(images:Union[List[Union[np.ndarray,str]],str,np.ndarray],ncols:int=3) -> None:
@@ -109,3 +111,65 @@ def draw_grid(im:np.ndarray, grid_size:int) -> np.ndarray:
     for j in range(0, im.shape[0], grid_size):
         cv2.line(im, (0, j), (im.shape[1], j), color=(255,))
     return im
+
+def read_image(image_path:str,shape:Union[List[int],Tuple[int,int,int]]) -> tf.Tensor:
+    '''
+    Read image from its path. Returns an uint8 tensor.
+    '''
+    if len(shape)!=3:
+        raise ValueError("The shape must be a tuple/list (width,height,channels)") 
+
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_png(image, channels=shape[2])
+    image.set_shape([None, None, shape[2]])
+    image = tf.image.resize(images=image, size=shape[0:2])
+    image = tf.cast(image, dtype=tf.uint8)
+
+    return image
+
+def color_map(mask:tf.Tensor) -> np.ndarray:
+    '''
+    Turn an 1-encoded gray image into a colored image
+    '''
+    background_color = (0,0,0) # black
+    clear_sky = (92, 179, 255) # clear blue
+    thin_cloud = (255, 243, 245) # shiny gray
+    tick_cloud = (113, 125, 150) # deep gray
+    sun = (255, 242, 0) # yellow
+    
+    colored_image = np.empty((mask.shape[0],mask.shape[1],3),dtype=np.uint8)
+    
+    
+    for class_index, color in enumerate([background_color,clear_sky,sun,tick_cloud,thin_cloud]):
+        indices = np.where(mask == class_index)
+        colored_image[indices[0],indices[1],:] = color
+    return colored_image
+
+def multipredict(img_list:List[str],mask_list:List[str],model:tf.keras.Model) -> None:
+    if len(img_list) != len(mask_list):
+        raise ValueError('There must be the same number os masks and images')
+    if len(img_list) == 0:
+        raise ValueError('There must be at least one image')
+    
+    fig, ax = plt.subplots(len(img_list),3,figsize=(12,4*len(img_list)+3))
+    index = 0 # row counter
+    for img_path, mask_path in zip(img_list,mask_list):
+        img = read_image(img_path,model.input.shape[1:])
+        ax[index,0].imshow( img )
+        ax[index,0].set_xticks([])
+        ax[index,0].set_yticks([])
+        
+        pred = model.predict( tf.expand_dims(img,0) )
+        ax[index,1].imshow( color_map(tf.argmax(pred[0,:,:,:],2)) )
+        ax[index,1].set_xticks([])
+        ax[index,1].set_yticks([])
+        
+        ax[index,2].imshow(color_map(read_image(mask_path,model.input.shape[1:])[:,:,0]))
+        ax[index,2].set_xticks([])
+        ax[index,2].set_yticks([])
+        
+        index += 1
+    
+    ax[0,0].set_title("Image")
+    ax[0,1].set_title("Inference")
+    ax[0,2].set_title("Ground truth")
